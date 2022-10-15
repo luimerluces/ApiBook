e069dc29c55f28e4cb2b8689f5549ea37da408eb
luimerluces <36189585+luimerluces@users.noreply.github.com>
2022-10-15 08:18:24 -0400

    15-10-2022

--- constante/constante.js
+++ constante/constante.js
@@ -7,13 +7,5 @@
     port:3306,    
     genSalt:10,
     secret: 'secretYankees*3322',
-    expiresIn: 80*80,
-    hostsmtp:'smtp.gmail.com',
-    portsmtp:'587',
-    userSendEmail:'notificaciones@atenasconsultores.com',
-    passwordSendMail:'Ll*123456789',
-    URLProyecto:'http://localhost/RetailScannig/',
-    connectionTimeout:900000,
-    requestTimeout:900000
-    //expiresIn: 60,
+    expiresIn: 80*80
 });
